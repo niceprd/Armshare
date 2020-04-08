04de0d590635deebbeb6f20f95f0c3c24d4d3e2c
Phamin Promcharoen <nice.phamin@gmail.com>
2020-04-08 16:32:22 +0700

    :cat: edit responsive page

--- js/functions/chat_popup.js
+++ js/functions/chat_popup.js
@@ -1,13 +1,15 @@
-$(document).ready(function() {
-    $("a.open-link").click(function() {
+$(document).ready(function () {
+    $("a.open-link").click(function () {
         $("div.pop-chat").toggleClass("active");
         $("div.pop-chat1").removeClass("active");
+        $("div.pop-chat2").removeClass("active");
     })
-    $("a.open-link1").click(function() {
+    $("a.open-link1").click(function () {
         $("div.pop-chat1").toggleClass("active");
         $("div.pop-chat").removeClass("active");
+        $("div.pop-chat2").removeClass("active");
     })
-    $("a.open-link2").click(function() {
+    $("a.open-link2").click(function () {
         $("div.pop-chat2").toggleClass("active");
         $("div.pop-chat").removeClass("active");
         $("div.pop-chat1").removeClass("active");
